289d0bac929fc5bd2c7c9902cea7ac7ab368a3ba
tmgraham2 <tmgraham2@aggies.ncat.edu>
2025-09-24 17:25:41 -0400

    Add files via upload
    # Pass Test Case 1: Initial game setup with required variables

---
+++ graham_assignment_4.py
@@ -0,0 +1,10 @@
+student_name = "Tashe Graham"
+current_gpa = 3.85
+study_hours = 20
+social_points = 30
+stress_level = 65
+print("Hello", f"{student_name}. These are your starting stats!")
+print("\nCurrent GPA:", current_gpa)
+print("\nStudy Hours:", study_hours)
+print("\nSocial Points:", social_points)
+print("\nStress Level:", stress_level)
